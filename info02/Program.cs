1d92c751a7995fa5014eced055258c7431743f2c
Ivan <chivvasregal94@gmail.com>
2023-02-14 14:44:55 +0300

    info02

---
+++ info02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите одно число");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while (index <= N)
+{
+    if (index % 2 == 0)
+    {
+        Console.WriteLine(index);
+    }
+    index = index + 1;
+ }
